b721e293a2bdd5abb29f19417f4f924c9699dc1c
aitorNeos <aitor.arque@tangelogames.com>
2019-01-17 17:50:45 +0100

    Ready for review

--- GolangSendGift.go
+++ GolangSendGift.go
@@ -1,37 +1,55 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
 )
 
-func main() 
-{
+type Log struct {
+	id   int    `json:"id"`
+	TimeStamp string `json:"TimeStamp"`
+	Message string `json:"Message"`
+	level string `json:"level"`
+	logger string `json:"logger"`
+	LoggingSourceMachine string `json:"LoggingSourceMachine"`
+	SessionId string `json:"SessionId"`
+	RemoteHost string `json:"RemoteHost"`
+}
+
+func main() {
     fmt.Println("Go MySQL first implementation")
     
-    // Open up our database connection.
-    // I've set up a database on my local machine using phpmyadmin.
-    // The database is called testDb
-    db, err := sql.Open("mysql", "root:password1@tcp(127.0.0.1:3306)/test")
+	var username = flag.String("username", "aitor", "MSql username")
+	var password = flag.String("password", "123456", "MSql user password")
+	flag.Parse()
+	
+    db, err := sql.Open("mysql", *username+ ":" + *password + "@tcp(172.20.2.65:3306)/xplogdb")
     
-    // if there is an error opening the connection, handle it
     if err != nil {
         panic(err.Error())
     }
     
-    // defer the close till after the main function has finished
-    // executing 
     defer db.Close()
     
-    // perform a db.Query insert 
-    insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+    query, err := db.Query("SELECT * FROM xplog x  WHERE x.Message LIKE '%UpdateBalance%'")
     
-    // if there is an error inserting, handle it
     if err != nil {
         panic(err.Error())
     }
+	
+	for query.Next() {
+		var log Log
+
+		err = query.Scan(&log.id, &log.TimeStamp,&log.Message,&log.level ,&log.logger ,&log.LoggingSourceMachine ,&log.SessionId,&log.RemoteHost)
+		if err != nil {
+			panic(err.Error()) 
+		}
+
+		fmt.Println(log.Message)
+	}
     // be careful deferring Queries if you are using transactions
-    defer insert.Close()
+    defer query.Close()
     
 }
